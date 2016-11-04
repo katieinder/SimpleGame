5d28d4420545fc8229e3c05ae8364d8c97f23b09
katieinder <inderk@cardiff.ac.uk>
2016-11-04 16:05:58 +0000

    Add files via upload
    This has the magnet in but the ball does not interact with it correctly.

--- SimpleGame.py
+++ SimpleGame.py
@@ -6,6 +6,8 @@ import pygame, sys
 #initialise pygame
 pygame.init()
 
+from GameObject import *
+
 #set the height and width of screen
 SCREEN_SIZE = [800,600]
 #open screen
@@ -18,10 +20,12 @@ pygame.mouse.set_visible(0)
 clock = pygame.time.Clock()
 
 #load resources
-Ball = pygame.image.load("Ball.png").convert_alpha()
-
+Ball = pygame.image.load("Ball.png")
 #get size
 BALL_SIZE=[22,22]
+
+Magnet = pygame.image.load("Magnet.png")
+MAGNET_SIZE = [100,25]
 
 #define x and y
 x,y = 0, 0
@@ -35,13 +39,13 @@ while 1:
 
     clock.tick(60)
 
+    screen.fill([250,250,250])
+
     for event in pygame.event.get():
         if event.type==pygame.QUIT:sys.exit()
 
-
-    screen.fill([250,250,250])
-
-    screen.blit(Ball,(x,y))
+    screen.blit(Ball, (x,y))
+    screen.blit(Magnet, (350,275))
 
     x += 2*x_direction
     y += 2*y_direction
@@ -50,6 +54,13 @@ while 1:
         x_direction *= -1
     if y + BALL_SIZE[1] > 600 or y <= 0:
         y_direction *=-1
+
+    if x + BALL_SIZE[0] >= 300 or 300 <= x <= 400:
+        x_direction *= -1
+
+    if y + BALL_SIZE[1] >= 250 or 250 <= y <= 275:
+        y_direction *= -1
+
 
 
     pygame.display.update()
